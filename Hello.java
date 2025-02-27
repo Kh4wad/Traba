13beda91b8ceb49613f7d71211ebb58595cef36b
Kh4wad <khawad@minha.fag.edu.br>
2025-02-26 21:13:07 -0300

    Coloquei ola mundo

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) throws Exception {
+        Caixa();
+    }
+    public static void Caixa() {
+        System.out.println("Ola mundo");
+    }
+}
